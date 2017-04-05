680b5693b268179dfd94b2d2c8d52014688facf8
Joe Drumgoole <joe.drumgoole@mongodb.com>
2017-04-05 17:36:41 +0100

    added a range of new functions

---
+++ python/_5_queries_and_projections.py
@@ -0,0 +1,129 @@
+'''
+Created on 5 Apr 2017
+
+@author: jdrumgoole
+'''
+import pymongo
+if __name__ == '__main__':
+    
+    client = pymongo.MongoClient()
+    database = client[ "b2b" ]
+    zips_collection = database[ "zips" ]
+    
+    print( 
+    '''
+A simple query for the first document in a collection
+    first = zips_collection.find_one()
+''')
+    
+    first = zips_collection.find_one()
+    print( first )
+    
+    print( 
+    '''
+Now query that returns a cursor. We must iterate the cursor 
+to return the documents. Note that the find does not send the query
+to the server until we begin to read the documents. So the first round 
+trip to the server happens on the first line of the for loop.
+
+    count = 0 
+    cursor = zips_collection.find()
+    for i in cursor: #server gets query
+        count = count + 1 
+        print( i )
+''')
+    count = 0 
+    cursor = zips_collection.find()
+    for i in cursor:
+        count = count + 1 
+        print( i )
+    print( "%i documents" % count )
+    
+    print(
+'''
+Now we want find a specific field by value. Lets find what city is at
+zip code "01036".
+
+    doc =  zips_collection.find_one( { "_id" : "01036" })
+''' 
+        )
+    
+    doc =  zips_collection.find_one( { "_id" : "01036" })
+    print( doc )
+    
+    print(
+'''
+What happens if the result set will be more than one document?
+
+    cursor = zips_collection.find( { "pop" : 426 })
+    count = 0
+    for i in cursor:
+        count = count + 1 
+        print( i )
+        
+If you call find_one the first document returned by the collection
+query will be produced. If you call find() then a cursor will be returned 
+that you can use to iterate over the complete result set.
+'''
+    )
+    
+    print( "Find one")
+    doc = zips_collection.find_one( { "pop" : 426 })
+    print( doc )
+    
+    print( "" )
+    
+    print( "Find many")
+    cursor = zips_collection.find( { "pop" : 426 })
+    count = 0
+    for i in cursor:
+        count = count + 1 
+        print( i )
+    print( "%i documents" % count )
+    
+    print(
+'''
+We can control the output with projections. Project tells the server to only 
+return a subset of the fields in a document.
+'''
+    )
+    
+    print( "Suppress the _id field")
+    cursor = zips_collection.find( { "pop" : 426 }, { "_id" : 0 })
+    count = 0
+    for i in cursor:
+        count = count + 1 
+        print( i )
+    print( "%i documents" % count )
+    
+    print( "Suppress the _id and loc fields")
+    cursor = zips_collection.find( { "pop" : 426 }, { "_id" : 0, "loc" : 0 })
+    count = 0
+    for i in cursor:
+        count = count + 1 
+        print( i )
+    print( "%i documents" % count )
+    
+    print(
+'''
+Explicitly include a field. Note once you have a field with a value of '1'
+then all other fields are excluded except other fields with a value of '1'.
+Note in this scenario the _id field will always be included unless it is
+explicitly excluded by setting it to _id : 0.
+''' )
+
+    print( "Just include the loc field")
+    cursor = zips_collection.find( { "pop" : 426 }, {  "loc" : 1 })
+    count = 0
+    for i in cursor:
+        count = count + 1 
+        print( i )
+    print( "%i documents" % count )
+    
+    print( "Just include the loc field and supress the _id field" )
+    cursor = zips_collection.find( { "pop" : 426 }, {  "_id" : 0, "loc" : 1 })
+    count = 0
+    for i in cursor:
+        count = count + 1 
+        print( i )
+    print( "%i documents" % count )
---
+++ python/_7_load_data.py
@@ -0,0 +1,49 @@
+'''
+Created on 5 Apr 2017
+
+@author: jdrumgoole
+'''
+
+if __name__ == '__main__':
+import requests
+import pymongo
+import json
+
+if __name__ == '__main__':
+    print( '''
+For this next set of examples we are going to use a public data set.
+Lets download the zips.json file. A collection of ZIP codes for the US
+in json format.
+
+This data is hosted on github at :
+
+https://github.com/jdrumgoole/back-to-basics-with-mongodb/tree/master/data
+    ''')
+    
+    print( "Downloading restaurants")
+    r = requests.get( "https://raw.githubusercontent.com/jdrumgoole/back-to-basics-with-mongodb/master/data/zips.json", stream=True)
+    with open("zips.json", 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
+    print( "Created zips.json")
+    
+    print( "Importing zips.json in mongodb")
+    client = pymongo.MongoClient()
+    b2b_database = client[ "b2b" ]
+    zips_collection = b2b_database[ "zips"]
+    
+    b2b_database.drop_collection( "zips" )
+    
+    count = 0
+    with open( "zips.json", "r") as fd:
+        for line in fd :
+            line = line[:-1] #clip \n
+            print( line )
+            doc = json.loads( line ) #convert to a dict object
+            zips_collection.insert_one( doc )
+            count = count + 1 
+            
+    print( "Inserted %i documents" % count )
+            
+            
+            
