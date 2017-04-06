3f2d9be40a65536a9b798f26f18c65b9a7fb8ed5
Joe Drumgoole <joe.drumgoole@mongodb.com>
2017-04-06 08:40:28 +0100

    merge

--- python/_2_createclient.py
+++ python/_2_createclient.py
@@ -24,6 +24,13 @@ if __name__ == '__main__':
     
     database = client["test"]
     
+    # 'ismaster' is the official commnand to ping the server. Until 
+    # ismaster is called the client has had not contact with the server
+    # the database will not be created on the server until this command executes.
+    #
+    # A database is a container for a set of collections. Each database is guaranteed to
+    # be stored in an independent collection of on disk files
+    #
     response = database.command("ismaster")
     
     pprint.pprint(response)
